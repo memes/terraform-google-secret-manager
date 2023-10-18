6e226326e5b40a6b4899f7e2427ceea312293008
Matthew Emes <memes@matthewemes.com>
2023-10-17 20:14:18 -0700

    chore: Bump version in docs and examples to 2.1.2

--- examples/user-managed-replication/main.tf
+++ examples/user-managed-replication/main.tf
@@ -11,7 +11,7 @@ terraform {
 
 module "secret" {
   source      = "memes/secret-manager/google"
-  version     = "2.1.1"
+  version     = "2.1.2"
   project_id  = var.project_id
   id          = var.id
   secret      = var.secret
